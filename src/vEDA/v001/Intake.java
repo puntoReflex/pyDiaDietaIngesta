4577b092d9f6a157d19c9abcf71d8d5e998e88be
manuel@-2.6 <manuel@masiasweb.com>
2024-02-15 11:42:07 +0100

    Cabeceras de métodos

--- src/vEDA/v001/Intake.java
+++ src/vEDA/v001/Intake.java
@@ -56,6 +56,9 @@ class Intake {
 
         Intake breakfast = new Intake();
         breakfast.createIntake();
+        breakfast.editIntake();
+        breakfast.deleteIntake();
+        breakfast.deleteAllIntake();
         breakfast.printFoodListing();
 
     }
